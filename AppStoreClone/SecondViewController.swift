5e7da0fb169c2c878c915055edbc589755575a9a
sy-hash <50n50n.xarc@gmail.com>
2018-02-26 02:53:02 +0900

    init commit

---
+++ AppStoreClone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AppStoreClone
+//
+//  Created by s-hashimoto on 2018/02/26.
+//  Copyright © 2018年 sy-hash. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
